080ee464ffdec18d7e63a36e96bbfb424a04a252
Andrea <andrea3aprile99@gmail.com>
2021-03-04 10:25:06 +0100

    ff

--- js/script.js
+++ js/script.js
@@ -8,5 +8,18 @@ inse=inse.toLowerCase();
 document.getElementById("demo").innerHTML = cognomi;
 cognomi.sort();
 document.getElementById("demo").innerHTML = cognomi;
+// soluzione1
 document.getElementById("demos").innerHTML =  "la posizione in lista del tuo cognome e' : " + cognomi.indexOf(inse);
+// soluzione2
+var i =0;
+var trovato =false;
+while (i < cognomi.lenght && trovato == false ) {
+  if (cognomi[i]==cognome){
+    trovato =true;
+  }
+  i++;
+}
+if (trovato == true){
+  consol.log(i);
+}
 // console.log(['🥚','🐔'].sort()[0] + ' was first!')
