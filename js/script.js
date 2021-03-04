1414c9176e0b9f6128e8c980b7587eafc12f18e9
Andrea <andrea3aprile99@gmail.com>
2021-03-04 10:05:58 +0100

    df

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@
 
 var cognomi = ["bianchi","rossi","inzaghi","madrigali","basilico","schianto","losanghe","corvati"];
 var inse =prompt("inserisci il tuo cognome");
+inse=inse.toLowerCase();
     cognomi.push(inse);
 document.getElementById("demo").innerHTML = cognomi;
 cognomi.sort();
 document.getElementById("demo").innerHTML = cognomi;
 document.getElementById("demos").innerHTML =  "la posizione in lista del tuo cognome e' : " + cognomi.indexOf(inse);
+// console.log(['🥚','🐔'].sort()[0] + ' was first!')
